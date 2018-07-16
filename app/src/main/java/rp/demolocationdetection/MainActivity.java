45674bf84d864e9f5bc8c113be0a8b26b6a60b11
16022582 <16022582@myrp.edu.sg>
2018-07-16 11:44:20 +0800

    finalise

--- app/src/main/java/rp/demolocationdetection/MainActivity.java
+++ app/src/main/java/rp/demolocationdetection/MainActivity.java
@@ -1,13 +1,75 @@
 package rp.demolocationdetection;
 
+import android.Manifest;
+import android.location.Location;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.content.PermissionChecker;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnGetLast;
+    FusedLocationProviderClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnGetLast = findViewById(R.id.button);
+
+        client = LocationServices.getFusedLocationProviderClient(this);
+
+        btnGetLast.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(checkPermission() == true){
+                    Task<Location> task = client.getLastLocation();
+                    task.addOnSuccessListener(MainActivity.this, new OnSuccessListener<Location>() {
+                        @Override
+                        public void onSuccess(Location location) {
+                            if(location != null){
+                                String message = "Lat: " + location.getLatitude() +
+                                        " Lng : " + location.getLongitude();
+                                Toast.makeText(MainActivity.this,message,Toast.LENGTH_SHORT).show();
+                            }
+
+                            else{
+                                String message = "No last known location";
+                                Toast.makeText(MainActivity.this,message,Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+
+                }
+            }
+        });
+
+
     }
+
+    private boolean checkPermission(){
+        int permissionCheck_Coarse = ContextCompat.checkSelfPermission(
+                MainActivity.this, Manifest.permission.ACCESS_COARSE_LOCATION);
+        int permissionCheck_Fine = ContextCompat.checkSelfPermission(
+                MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION);
+
+        if (permissionCheck_Coarse == PermissionChecker.PERMISSION_GRANTED
+                || permissionCheck_Fine == PermissionChecker.PERMISSION_GRANTED) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
 }
